f818f0f83306c5bf35a932ecb8ab86234583bf38
Brian <bhuan84497@gmail.com>
2026-07-08 11:23:39 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
+ // Function that gets a random choice for the computer
  function getComputerChoice() {
     let choiceRandomizer = Math.random();
 
     if (choiceRandomizer < 1/3) {
-        return "Rock";
+        return "rock";
     } else if (choiceRandomizer < 2/3) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
  }
 
+ // Function that gets a random choice for the human
  function getHumanChoice() {
     let humanChoice = prompt("Enter your input:");
 
-    return(humanChoice)
+    return(humanChoice.toLowerCase())
  }
 
-let humanScore;
-let computerScore;
+ // Function that plays a round
+function playRound(humanChoice, computerChoice) {
+    let win;
 
+    if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            win = true;
+        } else if (computerChoice == "paper") {
+            win = false;
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            win = true;
+        } else if (computerChoice == "scissors") {
+            win = false;
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "paper") {
+            win = true;
+        } else if (computerChoice == "rock") {
+            win = false;
+        }
+    }
+
+    console.log(computerChoice);
+
+    if (win) {
+        humanScore++;
+        console.log("You win!");
+    } else if (win === false) {
+        computerScore++;
+        console.log("You lose!");
+    } else {
+        console.log("You Tied!");
+    }
+
+    console.log("Your score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());

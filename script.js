58c28d90aa2d1dda1b5e2b4d9ff27b727361d9c6
Brian <bhuan84497@gmail.com>
2026-07-08 10:54:57 -0400

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+ function getComputerChoice() {
+
+    let choiceRandomizer = Math.random();
+
+    if (choiceRandomizer < 1/3) {
+        return "Rock";
+    } else if (choiceRandomizer < 2/3) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+ }
+
+function 
